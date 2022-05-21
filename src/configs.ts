94b35950d1d603795dbb3428a07d11b31fb1cc27
Carlos Santander <carlos.santander@gmail.com>
2022-05-21 03:17:10 -0500

    separate partidos

--- src/configs.ts
+++ src/configs.ts
@@ -13,22 +13,24 @@ dotenv.config();
 // Variables de la Aplicación //
 /*******************************************************************************************************/
 export const appEnvironment: string = process.env.APP_ENVIRONMENT || 'development'; // Entorno de la Aplicación
-export const appHost: string = process.env.APP_ELEC_HOST || 'localhost'; // Host de la Aplicación
+// export const appHost: string = process.env.APP_ELEC_HOST || 'localhost'; // Host de la Aplicación
+export const appHost: string = process.env.APP_ELEC_HOST_ || 'localhost'; // Host de la Aplicación
 export const appNombre: string = process.env.APP_ELEC_NAME || ''; // Nombre de la aplicación
 export const appDescripcion: string = process.env.APP_ELEC_DESCRIPTION || ''; // Descripción de la aplicación
-export const appPort: number = parseInt(process.env.APP_ELEC_PORT as string, 10) || 3000; // Puerto de la aplicación
+// export const appPort: number = parseInt(process.env.APP_ELEC_PORT as string, 10) || 3000; // Puerto de la aplicación
+export const appPort: number = parseInt(process.env.APP_ELEC_PORT_ as string, 10) || 3000; // Puerto de la aplicación
 export const appSecret: string = process.env.APP_ELEC_SECRET_TEXT || ''; // Texto secreto de la aplicación
 export const appAutorName: string = process.env.APP_ELEC_AUTHOR_NAME || ''; // Nombre o Compañia Autor de la aplicación
-
 /*******************************************************************************************************/
 // Variables de la Base de Datos MongoDB //
 /*******************************************************************************************************/
 export const dbDriver: string = process.env.APP_MONGO_DB_DRIV || 'mongodb'; // Driver de la base de datos
 export const dbHost: string = process.env.APP_MONGO_DB_HOST || 'localhost'; // Host de la base de datos
 export const dbPort: number = parseInt(process.env.APP_MONGO_DB_PORT as string, 10) || 27017; // Puerto de la base de datos
-export const dbName: string = process.env.APP_MONGO_DB_NAME_ELEC || 'test'; // Nombre de la base de datos
 export const dbUser: string = process.env.APP_MONGO_DB_USER || ''; // Usuario de la base de datos
 export const dbPwd: string = process.env.APP_MONGO_DB_PWD || ''; // Contraseña de la base de datos
+// export const dbName: string = process.env.APP_MONGO_DB_NAME_ELEC || 'test'; // Nombre de la base de datos
+export const dbName: string = process.env.APP_MONGO_DB_NAME_ELEC_ || 'test'; // Nombre de la base de datos
 
 /*******************************************************************************************************/
 // Configuraciones Generales de la Aplicación //
