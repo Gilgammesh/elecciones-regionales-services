2a4ce1c6abe4a9418ec5de507b4e06504cb35375
Carlos Santander <carlos.santander@gmail.com>
2022-06-07 18:50:35 -0500

    fix project

--- src/db/mongodb.ts
+++ src/db/mongodb.ts
@@ -1,49 +1,53 @@
 /*******************************************************************************************************/
 // Importamos las dependencias //
 /*******************************************************************************************************/
-import mongoose, { ConnectOptions } from 'mongoose';
-import 'colors';
-import { appEnvironment, dbDriver, dbHost, dbName, dbUser, dbPwd, dbPort } from '../configs';
+import mongoose, { ConnectOptions } from 'mongoose'
+import 'colors'
+import {
+  appEnvironment,
+  dbDriver,
+  dbHost,
+  dbName,
+  dbUser,
+  dbPwd,
+  dbPort
+} from '../configs'
 
 /*******************************************************************************************************/
 // Creamos la conexión a la Base de Datos MongoDB //
 /*******************************************************************************************************/
-const connection = async () => {
-	// Propiedades en la cadena de conexión
-	let properties: string = 'retryWrites=true&w=majority';
-	if (dbUser !== '' && dbPwd !== '') {
-		properties = 'authSource=admin&retryWrites=true&w=majority';
-	}
-	// URI de acceso a la Base de Datos
-	const URI: string = `${dbDriver}://${dbHost}:${dbPort}/${dbName}?${properties}`;
+export const connection = async () => {
+  // Propiedades en la cadena de conexión
+  let properties: string = 'retryWrites=true&w=majority'
+  if (dbUser !== '' && dbPwd !== '') {
+    properties = 'authSource=admin&retryWrites=true&w=majority'
+  }
+  // URI de acceso a la Base de Datos
+  const URI: string = `${dbDriver}://${dbHost}:${dbPort}/${dbName}?${properties}`
 
-	// Definimos las opciones de conexión a la base de datos
-	const mongooseOptions: ConnectOptions = {
-		// Construye index definidos en nuestros esquemas cuando se conecta (ideal en producción)
-		autoIndex: appEnvironment === 'development' ? true : false,
-		maxPoolSize: 10, // Mantener hasta n conexiones de sockets
-		serverSelectionTimeoutMS: 5000, // Continua intentando enviar operaciones durante n segundos
-		socketTimeoutMS: 45000, // Cierra sockets después de n segundos de inactividad.
-		family: 4, // Usa IPv4, Omite a IPv6
-		user: dbUser, // Pasamos el usuario
-		pass: dbPwd // Pasamos la contraseña
-	};
+  // Definimos las opciones de conexión a la base de datos
+  const mongooseOptions: ConnectOptions = {
+    // Construye index definidos en nuestros esquemas cuando se conecta (ideal en producción)
+    autoIndex: appEnvironment === 'development' ? true : false,
+    maxPoolSize: 10, // Mantener hasta n conexiones de sockets
+    serverSelectionTimeoutMS: 5000, // Continua intentando enviar operaciones durante n segundos
+    socketTimeoutMS: 45000, // Cierra sockets después de n segundos de inactividad.
+    family: 4, // Usa IPv4, Omite a IPv6
+    user: dbUser, // Pasamos el usuario
+    pass: dbPwd // Pasamos la contraseña
+  }
 
-	// Intentamos conectarnbos a la base de datos
-	try {
-		await mongoose.connect(URI, mongooseOptions);
-		console.log(`Conexión exitosa a MongoDB  `.green.bold.toString());
-		console.log(`💽 Base de datos: `.yellow.bold.toString() + `${dbName}   `.white.bold);
-		console.log('********************************************************************************'.rainbow);
-	} catch (error) {
-		// En caso de error mostramos
-		console.log('Error en la conexión:  '.white.bold);
-		console.log(error);
-		console.log('********************************************************************************'.rainbow);
-	}
-};
-
-/*******************************************************************************************************/
-// Exportamos la conexión a la base de datos por defecto //
-/*******************************************************************************************************/
-export default connection;
+  // Intentamos conectarnbos a la base de datos
+  try {
+    await mongoose.connect(URI, mongooseOptions)
+    console.log(
+      `💽 MongoDB =>`.green.bold +
+        ` Base de datos: `.yellow.bold.toString() +
+        `${dbName}   `.white.bold
+    )
+  } catch (error) {
+    // En caso de error mostramos
+    console.log('Error en la conexión:  '.white.bold)
+    console.log(error)
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,42 @@
 /***************************************************************************************************************/
 // Importamos las dependencias //
 /***************************************************************************************************************/
-import { createServer } from 'http';
-import { platform } from 'os';
-import { textSync } from 'figlet';
-import 'moment/locale/es';
-import 'colors';
-import app from './app';
-import mongodb from './db/mongodb';
-import sockets from './sockets';
-import { appAutorName, appEnvironment, appHost, appNombre, appPort } from './configs';
+import { createServer } from 'http'
+import { platform } from 'os'
+import 'moment/locale/es'
+import 'colors'
+import app from './app'
+import { connection as mongoDbConn } from './db/mongodb'
+import sockets from './sockets'
+import { appEnvironment, appHost, appNombre, appPort } from './configs'
 
 /***************************************************************************************************************/
 // Creamos el Servidor HTTP //
 /***************************************************************************************************************/
-const httpServer = createServer(app);
+const httpServer = createServer(app)
 
 /***************************************************************************************************************/
 // Inicializamos el Servidor Socket IO //
 /***************************************************************************************************************/
-sockets(httpServer);
+sockets(httpServer)
 
 /***************************************************************************************************************/
 // Arrancamos el Servidor HTTP con Express //
 /***************************************************************************************************************/
 httpServer.listen(appPort, () => {
-	console.log('********************************************************************************'.rainbow);
-	console.log(textSync(appAutorName).blue.bold);
-	console.log('********************************************************************************'.rainbow);
-	console.log(`*** ${appNombre} ***`.cyan.bold);
-	console.log('********************************************************************************'.rainbow);
-	if (appEnvironment === 'development') {
-		console.log(
-			`🚀 ${platform().toUpperCase()} Servidor (${appEnvironment}) => Listo en: `.yellow.bold.toString() +
-				`${appHost}:${appPort}   `.white.bold
-		);
-	}
-	if (appEnvironment === 'production') {
-		console.log(
-			`🚀 ${platform().toUpperCase()} Servidor (${appEnvironment}), listo en: `.yellow.bold.toString() +
-				`${appHost}   `.white.bold
-		);
-	}
-	console.log('********************************************************************************'.rainbow);
-	// Nos conectamos a la Base de Datos MongoDB
-	mongodb();
-});
+  console.log(`*** ${appNombre.toLocaleUpperCase()} ***`.cyan.bold)
+  if (appEnvironment === 'development') {
+    console.log(
+      `🚀 ${platform().toUpperCase()} Servidor (${appEnvironment}) => Listo en: `.yellow.bold.toString() +
+        `${appHost}:${appPort}   `.white.bold
+    )
+  }
+  if (appEnvironment === 'production') {
+    console.log(
+      `🚀 ${platform().toUpperCase()} Servidor (${appEnvironment}), listo en: `.yellow.bold.toString() +
+        `${appHost}   `.white.bold
+    )
+  }
+  // Nos conectamos a la Base de Datos MongoDB
+  mongoDbConn()
+})
